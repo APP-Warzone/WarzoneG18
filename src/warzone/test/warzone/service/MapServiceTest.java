6de9aaea09c53a79036700c77022d0a940c88ae1
LoveBorad15 <LOVEBORAD15@GMAIL.COM>
2024-02-17 15:20:01 -0500

    Update MapServiceTest.java
    validateContinentMap function implemented

--- src/warzone/test/warzone/service/MapServiceTest.java
+++ src/warzone/test/warzone/service/MapServiceTest.java
@@ -56,4 +56,63 @@ public class MapServiceTest {
         System.out.println("Number of Continents: " + gameContext.getContinents().size());
         System.out.println("Number of Countries: " + gameContext.getCountries().size());
     }
+
+    /**
+     * continentMap1 is a connected subgraph
+     */
+    @Test
+    public void validateContinentMap1() {
+        // set map into d_gamecontext
+        d_gameContext.getContinents().put(1, new Continent(1, "North_America", 3, Color.RED));
+        d_gameContext.getContinents().put(2, new Continent(2, "South_America", 5, Color.BLUE));
+
+        CountryController _countryCtrl = new CountryController(d_gameContext);
+        Country canada = new Country(1, "Canada", 343, 435, d_gameContext.getContinents().get(1));
+        _countryCtrl.addCountry(1, 1);
+
+        Country us = new Country(2, "US", 343, 435, d_gameContext.getContinents().get(1));
+        _countryCtrl.addCountry(2, 1);
+
+        Country mexico = new Country(3, "mexico", 343, 435, d_gameContext.getContinents().get(1));
+        _countryCtrl.addCountry(3, 1);
+
+        NeighborController _nbCtrl = new NeighborController(d_gameContext);
+        _nbCtrl.addNeighbor(1, 2);
+        _nbCtrl.addNeighbor(2, 1);
+        _nbCtrl.addNeighbor(2, 3);
+        _nbCtrl.addNeighbor(3, 2);
+
+        MapService d_mapService = new MapService(d_gameContext);
+        Continent continent = d_gameContext.getContinents().get(1);
+        assertTrue(d_mapService.validateSubGraph(continent));
+    }
+
+    /**
+     * continentMap2 is not a connected subgraph
+     */
+    @Test
+    public void validateContinentMap2() {
+        // set map into d_gamecontext
+        d_gameContext.getContinents().put(1, new Continent(1, "North_America", 3, Color.RED));
+        d_gameContext.getContinents().put(2, new Continent(2, "South_America", 5, Color.BLUE));
+
+        CountryController _countryCtrl = new CountryController(d_gameContext);
+        Country canada = new Country(1, "Canada", 343, 435, d_gameContext.getContinents().get(1));
+        _countryCtrl.addCountry(1, 1);
+
+        Country us = new Country(2, "US", 343, 435, d_gameContext.getContinents().get(1));
+        _countryCtrl.addCountry(2, 1);
+
+        Country mexico = new Country(3, "mexico", 343, 435, d_gameContext.getContinents().get(1));
+        _countryCtrl.addCountry(3, 1);
+
+        NeighborController _nbCtrl = new NeighborController(d_gameContext);
+        _nbCtrl.addNeighbor(1, 2);
+        _nbCtrl.addNeighbor(2, 1);
+        _nbCtrl.addNeighbor(2, 3);
+
+        MapService d_mapService = new MapService(d_gameContext);
+        Continent continent = d_gameContext.getContinents().get(1);
+        assertFalse(d_mapService.validateSubGraph(continent));
+    }
 }
