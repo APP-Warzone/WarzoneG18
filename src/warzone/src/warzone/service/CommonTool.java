cdb3475153d42b9a8365ac595810934abfb346b6
harsh-hirapara <harshhirapara2000@gmail.com>
2024-03-14 21:39:18 -0400

    add command parsor in services files

--- src/warzone/src/warzone/service/CommonTool.java
+++ src/warzone/src/warzone/service/CommonTool.java
@@ -1,27 +1,40 @@
 package warzone.service;
 
+import warzone.view.GenericView;
+
 public class CommonTool {
-
-    public static int parseInt(String p_number) {
-        int l_result = -1;
-        if(p_number != null) {
-            try {
-                l_result = Integer.parseInt(p_number.trim());
-            }
-            catch(Exception ex) {
-            }
-        }
-        return l_result;
-    }
-
-    public static String[] conventToArray(String p_parameters) {
-        if(p_parameters != null)
-            return p_parameters.split("\\s+");
-        else
-            return new String[] {};
-    }
-
-    public static int getRandomNumber(int p_minNumber, int p_maxNumber) {
-        return (int) ((Math.random() * (p_maxNumber - p_minNumber)) + p_minNumber);
-    }
+	
+	public static int parseInt(String p_number) {
+		int l_result = -1;
+		if(p_number != null) {
+			try {
+				 l_result = Integer.parseInt(p_number.trim());
+			}
+			catch(Exception ex) {
+			}
+		}
+		return l_result;		
+	}
+	
+	public static String[] conventToArray(String p_parameters) {
+		if(p_parameters != null)
+			return p_parameters.split("\\s+");
+		else
+			return new String[] {};
+	}	
+	
+	public static int getRandomNumber(int p_minNumber, int p_maxNumber) {
+	    return (int) ((Math.random() * (p_maxNumber - p_minNumber)) + p_minNumber);
+	}
+	
+	public static String convertArray2String(String[] p_stringArray, String p_separator, int from, int to) {
+		StringBuilder l_sb = new StringBuilder();
+		for (int i = from; i < to + 1; i++) {
+			l_sb.append(p_stringArray[i]);
+			if (i < to && i < p_stringArray.length - 1) {
+				l_sb.append(p_separator);
+			}
+		}
+		return l_sb.toString();
+	}
 }
--- src/warzone/src/warzone/service/ControllerFactory.java
+++ src/warzone/src/warzone/service/ControllerFactory.java
@@ -3,65 +3,78 @@ package warzone.service;
 import warzone.controller.*;
 import warzone.model.GameContext;
 
-public class ControllerFactory {
-
+public class ControllerFactory {	
+	
 	private static ControllerFactory CONTROLLER_FACTORY;
 	private GameContext d_gameContext;
-
+	
 	private CommonController d_commonController;
 	private ContinentController d_continentController;
 	private MapController d_mapController;
 	private CountryController d_countryController;
 	private NeighborController d_neighborController;
+	private StartupController d_startupController;
+	private ErrorController d_errorController;
 	private GameplayController d_gameplayController;
-
-	private ControllerFactory()	{
+	
+	private ControllerFactory()	{	
 		d_gameContext = GameContext.getGameContext();
 	}
-
-
+	
 	public static ControllerFactory getControllerFactory() {
 		if( CONTROLLER_FACTORY == null)
 			CONTROLLER_FACTORY = new ControllerFactory();
 
 		return CONTROLLER_FACTORY;
 	}
-
+	
 	public CommonController getCommonController() {
 		if(d_commonController == null)
 			d_commonController = new CommonController(d_gameContext);
 		return d_commonController;
 	};
-
+	
 	public ContinentController getContinentController() {
 		if(d_continentController == null)
 			d_continentController = new ContinentController(d_gameContext);
 		return d_continentController;
 	};
-
+	
 	public MapController getMapController() {
 		if(d_mapController == null)
 			d_mapController = new MapController(d_gameContext);
 		return d_mapController;
 	};
-
+	
 	public CountryController getCountryController() {
 		if(d_countryController == null)
 			d_countryController = new CountryController(d_gameContext);
 		return d_countryController;
 	};
-
+	
 	public NeighborController getNeighborController() {
 		if(d_neighborController == null)
 			d_neighborController = new NeighborController(d_gameContext);
 		return d_neighborController;
 	};
-
+	
+	public StartupController getStartupController() {
+		if(d_startupController == null)
+			d_startupController = new StartupController(d_gameContext);
+		return d_startupController;
+	};
+	
 	public GameplayController getGameplayController() {
 		if(d_gameplayController == null)
 			d_gameplayController = new GameplayController(d_gameContext);
 		return d_gameplayController;
 	};
-
-
+	
+	
+	public ErrorController getErrorController() {
+		if(d_errorController == null)
+			d_errorController = new ErrorController();
+		return d_errorController;
+	};
+	
 }
