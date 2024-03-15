01579f478e6a66c7dd1e1ed4e1edd4bc85d8ec09
harsh-hirapara <harshhirapara2000@gmail.com>
2024-03-15 10:23:05 -0400

    update command scanner

--- src/warzone/src/warzone/controller/MapController.java
+++ src/warzone/src/warzone/controller/MapController.java
@@ -17,7 +17,7 @@ public class MapController {
 	private GameContext d_gameContext;
 
 	public MapController(GameContext p_gameContext) {
-
+		
 		d_gameContext = p_gameContext;
 		d_mapService = new MapService(p_gameContext);
 	}
@@ -29,23 +29,23 @@ public class MapController {
 	 */
 	public void showMap () {
 
-		MapView.printMap(d_gameContext.getContinents());
+		MapView.printMap(d_gameContext);
 	}
-
+	
 	/**
 	 * Performs the action for the user command: savemap filename
-	 *
+	 * 
 	 * Save a map to a text file exactly as edited (using the "domination" game map format).
-	 * @throws IOException
+	 * @throws IOException 
 	 */
 	public boolean saveMap (String p_fileName) throws IOException {
-
+		
 		// validate if the filename is legal
 		if(p_fileName == null || p_fileName.trim().isEmpty() || p_fileName.trim().length() > 20 ) {
 			GenericView.printError("InValid File Name, please type a valid file name, with length less than 20.");
 			return false;
 		}
-
+		
 		// call mapService to save the map and return the path
 		p_fileName = p_fileName.trim();
 		try{
@@ -55,37 +55,41 @@ public class MapController {
 			}
 			else {
 				GenericView.printError("Exception occured when saving the map, please valid the file name or contact the Administrator.");
-				return false;
-			}
+				return false;	
+			}		
 		}
 		catch(Exception ex) {
 			GenericView.printError("Exception occured when saving the map. " + ex.toString());
 			return false;
 		}
 	}
-
+	
 	/**
 	 * Performs the action for the user command: editmap filename
-	 *
+	 * 
 	 * Load a map from an existing "domination" map file, or create a new map from scratch if the file does not exist
 	 */
 	public boolean editMap (String p_fileName) {
-
+		
 		return d_mapService.editMap(p_fileName);
 	}
-
+	
 	/**
 	 * Performs the action for the user command: validatemap
-	 *
-	 * Verification of map correctness. The map should be automatically validated upon loading
-	 * and before saving (at least 3 types of incorrect maps). The validatemap command can be
-	 * triggered any time during map editing.
+	 * 
+	 * Verification of map correctness. The map should be automatically validated upon loading 
+	 * and before saving (at least 3 types of incorrect maps). The validatemap command can be 
+	 * triggered any time during map editing. 
 	 */
-	public boolean validateMap () {
-
-		// TODO Auto-generated method stub
-
-		return false;
+	public boolean validateMap () {		
+		if(! d_mapService.validateMap(d_gameContext) ) {
+			GenericView.printError("It is not a connected map.");
+			return false;
+		}
+		else {
+			GenericView.printSuccess("Yeah! You got a connected map!");
+			return true;
+		}
 	}
-
+	
 }
--- src/warzone/src/warzone/service/ContinentService.java
+++ src/warzone/src/warzone/service/ContinentService.java
@@ -5,31 +5,37 @@ import java.util.Map;
 import warzone.model.*;
 
 public class ContinentService {
+	
+	private GameContext d_gameContext;
 
-    private GameContext d_gameContext;
-
-    public ContinentService(GameContext p_gameContext) {
-        d_gameContext = p_gameContext;
-    }
-
-    public boolean add(Continent p_continent) {
-        //0. add the item to
-        if(p_continent != null) {
-            Map<Integer,Continent> l_continents=d_gameContext.getContinents();
-            l_continents.put(p_continent.getContinentID(), p_continent);
-            return true;
-        }
-        return false;
-    }
-
-    public boolean remove(int p_continentID) {
-        if(p_continentID > 0) {
-            Map<Integer,Continent> l_continents=d_gameContext.getContinents();
-            if(l_continents.containsKey(p_continentID)){
-                l_continents.remove(p_continentID);
-                return true;
-            }
-        }
-        return false;
-    }
+	public ContinentService(GameContext p_gameContext) {
+		d_gameContext = p_gameContext;
+	}
+	
+	public boolean add(Continent p_continent) {
+		//0. add the item to
+		if(p_continent != null) {
+			Map<Integer,Continent> l_continents=d_gameContext.getContinents();			
+			l_continents.put(p_continent.getContinentID(), p_continent);
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean remove(int p_continentID) {
+		if(p_continentID > 0) {
+			Map<Integer,Continent> l_continents=d_gameContext.getContinents();
+			if(l_continents.containsKey(p_continentID)){
+				Continent l_continent = l_continents.get(p_continentID);
+				//remove from continent list
+		        for (Country l_tempCountry : l_continent.getCountries().values())
+		        	l_tempCountry.setContinent(null);        
+		        
+				//remove from the country reference
+				l_continents.remove(p_continentID);
+				return true;
+			}			
+		}
+		return false;
+	}
 }
