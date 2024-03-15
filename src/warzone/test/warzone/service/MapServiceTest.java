7568f00bfe76451c8b48a00051d7a565e0cf8661
LoveBorad15 <LOVEBORAD15@GMAIL.COM>
2024-03-15 14:55:10 -0400

    Naming Convention
    Naming Convention

--- src/warzone/test/warzone/service/MapServiceTest.java
+++ src/warzone/test/warzone/service/MapServiceTest.java
@@ -41,20 +41,20 @@ public class MapServiceTest {
 	@Test
 	public void testEditMap() {
 		
-		GameContext gameContext = GameContext.getGameContext();
-		MapController mapController = new MapController(gameContext);
+		GameContext l_gameContext = GameContext.getGameContext();
+		MapController l_mapController = new MapController(l_gameContext);
 		
-		assertTrue(mapController.editMap("europe.map"));
+		assertTrue(l_mapController.editMap("europe.map"));
 		
-		System.out.println("Map File Name: " + gameContext.getMapFileName());
-		System.out.println("Map File Pic: " + gameContext.getMapFilePic());
-		System.out.println("Map File Map: " + gameContext.getMapFileMap());
-		System.out.println("Map File Name: " + gameContext.getMapFileCards());
+		System.out.println("Map File Name: " + l_gameContext.getMapFileName());
+		System.out.println("Map File Pic: " + l_gameContext.getMapFilePic());
+		System.out.println("Map File Map: " + l_gameContext.getMapFileMap());
+		System.out.println("Map File Name: " + l_gameContext.getMapFileCards());
 		
 		System.out.println();
 		
-		System.out.println("Number of Continents: " + gameContext.getContinents().size());
-		System.out.println("Number of Countries: " + gameContext.getCountries().size());
+		System.out.println("Number of Continents: " + l_gameContext.getContinents().size());
+		System.out.println("Number of Countries: " + l_gameContext.getCountries().size());
 	}
 	
     /**
@@ -64,53 +64,52 @@ public class MapServiceTest {
     public void validateMap1() {
 
         //set map into d_gamecontext
-        d_gameContext.getContinents().put(1,new Continent(1,"North_America", 3, Color.RED ));
-        d_gameContext.getContinents().put(2,new Continent(2,"South_America", 5, Color.BLUE ));
+        d_gameContext.getContinents().put(1,new Continent(1,"North_America", 3, "RED" ));
+        d_gameContext.getContinents().put(2,new Continent(2,"South_America", 5, "BLUE" ));
 
-        CountryController _countryCtrl = new CountryController(d_gameContext);
-        Country canada = new Country(1,"Canada", 343,435, d_gameContext.getContinents().get(1) );
-        _countryCtrl.addCountry(1,1);
+        CountryController l_countryCtrl = new CountryController(d_gameContext);
+        new Country(1,"Canada", 343,435, d_gameContext.getContinents().get(1) );
+        l_countryCtrl.addCountry(1,1);
 
-        Country us = new Country(2,"US", 343,435, d_gameContext.getContinents().get(1) );
-        _countryCtrl.addCountry(2,1);
+        new Country(2,"US", 343,435, d_gameContext.getContinents().get(1) );
+        l_countryCtrl.addCountry(2,1);
 
-        Country mexco = new Country(3,"mexco", 343,435, d_gameContext.getContinents().get(1) );
-        _countryCtrl.addCountry(3,1);
+        new Country(3,"mexco", 343,435, d_gameContext.getContinents().get(1) );
+        l_countryCtrl.addCountry(3,1);
 
-        Country cuba = new Country(4,"cuba", 343,435, d_gameContext.getContinents().get(2) );
-        _countryCtrl.addCountry(4,2);
+        new Country(4,"cuba", 343,435, d_gameContext.getContinents().get(2) );
+        l_countryCtrl.addCountry(4,2);
 
-        Country brazil = new Country(5,"brazil", 343,435, d_gameContext.getContinents().get(2) );
-        _countryCtrl.addCountry(5,2);
+        new Country(5,"brazil", 343,435, d_gameContext.getContinents().get(2) );
+        l_countryCtrl.addCountry(5,2);
 
-        Country chili = new Country(6,"chili", 343,435, d_gameContext.getContinents().get(2) );
-        _countryCtrl.addCountry(6,2);
+        new Country(6,"chili", 343,435, d_gameContext.getContinents().get(2) );
+        l_countryCtrl.addCountry(6,2);
 
-        Country agentina = new Country(7,"agentina", 343,435, d_gameContext.getContinents().get(2) );
-        _countryCtrl.addCountry(7,2);
+        new Country(7,"agentina", 343,435, d_gameContext.getContinents().get(2) );
+        l_countryCtrl.addCountry(7,2);
 
-        NeighborController _nbCtrl = new NeighborController(d_gameContext);
-        _nbCtrl.addNeighbor(1,2);
-        _nbCtrl.addNeighbor(2,1);
-        _nbCtrl.addNeighbor(2,3);
-        _nbCtrl.addNeighbor(3,2);
+        NeighborController l_nbCtrl = new NeighborController(d_gameContext);
+        l_nbCtrl.addNeighbor(1,2);
+        l_nbCtrl.addNeighbor(2,1);
+        l_nbCtrl.addNeighbor(2,3);
+        l_nbCtrl.addNeighbor(3,2);
 
-        _nbCtrl.addNeighbor(3,5);
-        _nbCtrl.addNeighbor(5,3);
+        l_nbCtrl.addNeighbor(3,5);
+        l_nbCtrl.addNeighbor(5,3);
 
-        _nbCtrl.addNeighbor(4,5);
-        _nbCtrl.addNeighbor(4,6);
-        _nbCtrl.addNeighbor(4,7);
-        _nbCtrl.addNeighbor(5,7);
-        _nbCtrl.addNeighbor(5,6);
-        _nbCtrl.addNeighbor(5,4);
-        _nbCtrl.addNeighbor(6,7);
-        _nbCtrl.addNeighbor(6,4);
-        _nbCtrl.addNeighbor(7,6);
+        l_nbCtrl.addNeighbor(4,5);
+        l_nbCtrl.addNeighbor(4,6);
+        l_nbCtrl.addNeighbor(4,7);
+        l_nbCtrl.addNeighbor(5,7);
+        l_nbCtrl.addNeighbor(5,6);
+        l_nbCtrl.addNeighbor(5,4);
+        l_nbCtrl.addNeighbor(6,7);
+        l_nbCtrl.addNeighbor(6,4);
+        l_nbCtrl.addNeighbor(7,6);
 
-        MapService d_mapService = new MapService(d_gameContext);
-        boolean r = d_mapService.validateMap(d_gameContext);
-        assertTrue(r);
+        MapService l_mapService = new MapService(d_gameContext);
+        assertTrue(l_mapService.validateMap(d_gameContext));
     }
     
 	/**
@@ -120,14 +119,14 @@ public class MapServiceTest {
     public void validateMap2() {
 
         //add map info into d_gameContext
-        d_gameContext.getContinents().put(1,new Continent(1,"North_America", 3, Color.RED ));
+        d_gameContext.getContinents().put(1,new Continent(1,"North_America", 3, "RED" ));
 
-        CountryController _countryCtrl = new CountryController(d_gameContext);
-        Country canada = new Country(1,"Canada", 343,435, d_gameContext.getContinents().get(1) );
-        _countryCtrl.addCountry(1,1);
+        CountryController l_countryCtrl = new CountryController(d_gameContext);
+        new Country(1,"Canada", 343,435, d_gameContext.getContinents().get(1) );
+        l_countryCtrl.addCountry(1,1);
 
-        MapService d_mapService = new MapService(d_gameContext);
-        assertFalse(d_mapService.validateMap(d_gameContext));
+        MapService l_mapService = new MapService(d_gameContext);
+        assertFalse(l_mapService.validateMap(d_gameContext));
     }
     
 	/**
@@ -137,23 +136,23 @@ public class MapServiceTest {
     public void validateMap3() {
 
         //add map info into d_gameContext
-        d_gameContext.getContinents().put(1,new Continent(1,"North_America", 3, Color.RED ));
-        d_gameContext.getContinents().put(3,new Continent(2,"Asia", 5, Color.PINK));
+        d_gameContext.getContinents().put(1,new Continent(1,"North_America", 3, "RED" ));
+        d_gameContext.getContinents().put(3,new Continent(2,"Asia", 5, "PINK"));
 
-        CountryController _countryCtrl = new CountryController(d_gameContext);
-        Country canada = new Country(1,"Canada", 343,435, d_gameContext.getContinents().get(1) );
-        _countryCtrl.addCountry(1,1);
+        CountryController l_countryCtrl = new CountryController(d_gameContext);
+        new Country(1,"Canada", 343,435, d_gameContext.getContinents().get(1) );
+        l_countryCtrl.addCountry(1,1);
 
 
-        Country us = new Country(2,"US", 343,435, d_gameContext.getContinents().get(1) );
-        _countryCtrl.addCountry(2,1);
+        new Country(2,"US", 343,435, d_gameContext.getContinents().get(1) );
+        l_countryCtrl.addCountry(2,1);
 
-        NeighborController _nbCtrl = new NeighborController(d_gameContext);
-        _nbCtrl.addNeighbor(1,2);
-        _nbCtrl.addNeighbor(2,1);
+        NeighborController l_nbCtrl = new NeighborController(d_gameContext);
+        l_nbCtrl.addNeighbor(1,2);
+        l_nbCtrl.addNeighbor(2,1);
 
-        MapService d_mapService = new MapService(d_gameContext);
-        assertFalse(d_mapService.validateMap(d_gameContext));
+        MapService l_mapService = new MapService(d_gameContext);
+        assertFalse(l_mapService.validateMap(d_gameContext));
     }
 
     /**
@@ -163,50 +162,50 @@ public class MapServiceTest {
     public void validateMap4() {
 
         //set map into d_gamecontext
-        d_gameContext.getContinents().put(1,new Continent(1,"North_America", 3, Color.RED ));
-        d_gameContext.getContinents().put(2,new Continent(2,"South_America", 5, Color.BLUE ));
+        d_gameContext.getContinents().put(1,new Continent(1,"North_America", 3, "RED" ));
+        d_gameContext.getContinents().put(2,new Continent(2,"South_America", 5, "BLUE" ));
 
-        CountryController _countryCtrl = new CountryController(d_gameContext);
-        Country canada = new Country(1,"Canada", 343,435, d_gameContext.getContinents().get(1) );
-        _countryCtrl.addCountry(1,1);
+        CountryController l_countryCtrl = new CountryController(d_gameContext);
+        new Country(1,"Canada", 343,435, d_gameContext.getContinents().get(1) );
+        l_countryCtrl.addCountry(1,1);
 
-        Country us = new Country(2,"US", 343,435, d_gameContext.getContinents().get(1) );
-        _countryCtrl.addCountry(2,1);
+        new Country(2,"US", 343,435, d_gameContext.getContinents().get(1) );
+        l_countryCtrl.addCountry(2,1);
 
-        Country mexco = new Country(3,"mexco", 343,435, d_gameContext.getContinents().get(1) );
-        _countryCtrl.addCountry(3,1);
+        new Country(3,"mexco", 343,435, d_gameContext.getContinents().get(1) );
+        l_countryCtrl.addCountry(3,1);
 
-        Country cuba = new Country(4,"cuba", 343,435, d_gameContext.getContinents().get(2) );
-        _countryCtrl.addCountry(4,2);
+        new Country(4,"cuba", 343,435, d_gameContext.getContinents().get(2) );
+        l_countryCtrl.addCountry(4,2);
 
-        Country brazil = new Country(5,"brazil", 343,435, d_gameContext.getContinents().get(2) );
-        _countryCtrl.addCountry(5,2);
+        new Country(5,"brazil", 343,435, d_gameContext.getContinents().get(2) );
+        l_countryCtrl.addCountry(5,2);
 
-        Country chili = new Country(6,"chili", 343,435, d_gameContext.getContinents().get(2) );
-        _countryCtrl.addCountry(6,2);
+        new Country(6,"chili", 343,435, d_gameContext.getContinents().get(2) );
+        l_countryCtrl.addCountry(6,2);
 
-        Country agentina = new Country(7,"agentina", 343,435, d_gameContext.getContinents().get(2) );
-        _countryCtrl.addCountry(7,2);
+        new Country(7,"agentina", 343,435, d_gameContext.getContinents().get(2) );
+        l_countryCtrl.addCountry(7,2);
 
-        NeighborController _nbCtrl = new NeighborController(d_gameContext);
-        _nbCtrl.addNeighbor(1,2);
-        _nbCtrl.addNeighbor(2,1);
-        _nbCtrl.addNeighbor(2,3);
-        _nbCtrl.addNeighbor(3,2);
+        NeighborController l_nbCtrl = new NeighborController(d_gameContext);
+        l_nbCtrl.addNeighbor(1,2);
+        l_nbCtrl.addNeighbor(2,1);
+        l_nbCtrl.addNeighbor(2,3);
+        l_nbCtrl.addNeighbor(3,2);
 
-        _nbCtrl.addNeighbor(3,5);
-        _nbCtrl.addNeighbor(5,3);
+        l_nbCtrl.addNeighbor(3,5);
+        l_nbCtrl.addNeighbor(5,3);
 
-        _nbCtrl.addNeighbor(4,5);
-        _nbCtrl.addNeighbor(4,6);
-        _nbCtrl.addNeighbor(4,7);
-        _nbCtrl.addNeighbor(5,7);
-        _nbCtrl.addNeighbor(5,6);
-        _nbCtrl.addNeighbor(5,4);
-        _nbCtrl.addNeighbor(6,7);
+        l_nbCtrl.addNeighbor(4,5);
+        l_nbCtrl.addNeighbor(4,6);
+        l_nbCtrl.addNeighbor(4,7);
+        l_nbCtrl.addNeighbor(5,7);
+        l_nbCtrl.addNeighbor(5,6);
+        l_nbCtrl.addNeighbor(5,4);
+        l_nbCtrl.addNeighbor(6,7);
 
-        MapService d_mapService = new MapService(d_gameContext);
-        assertFalse(d_mapService.validateMap(d_gameContext));
+        MapService l_mapService = new MapService(d_gameContext);
+        assertFalse(l_mapService.validateMap(d_gameContext));
     }
 
     /**
@@ -215,28 +214,28 @@ public class MapServiceTest {
     @Test
     public void validateContinentMap1() {
         //set map into d_gamecontext
-        d_gameContext.getContinents().put(1,new Continent(1,"North_America", 3, Color.RED ));
-        d_gameContext.getContinents().put(2,new Continent(2,"South_America", 5, Color.BLUE ));
+        d_gameContext.getContinents().put(1,new Continent(1,"North_America", 3, "RED" ));
+        d_gameContext.getContinents().put(2,new Continent(2,"South_America", 5, "BLUE" ));
 
-        CountryController _countryCtrl = new CountryController(d_gameContext);
-        Country canada = new Country(1,"Canada", 343,435, d_gameContext.getContinents().get(1) );
-        _countryCtrl.addCountry(1,1);
+        CountryController l_countryCtrl = new CountryController(d_gameContext);
+        new Country(1,"Canada", 343,435, d_gameContext.getContinents().get(1) );
+        l_countryCtrl.addCountry(1,1);
 
-        Country us = new Country(2,"US", 343,435, d_gameContext.getContinents().get(1) );
-        _countryCtrl.addCountry(2,1);
+        new Country(2,"US", 343,435, d_gameContext.getContinents().get(1) );
+        l_countryCtrl.addCountry(2,1);
 
-        Country mexco = new Country(3,"mexco", 343,435, d_gameContext.getContinents().get(1) );
-        _countryCtrl.addCountry(3,1);
+        new Country(3,"mexco", 343,435, d_gameContext.getContinents().get(1) );
+        l_countryCtrl.addCountry(3,1);
 
-        NeighborController _nbCtrl = new NeighborController(d_gameContext);
-        _nbCtrl.addNeighbor(1,2);
-        _nbCtrl.addNeighbor(2,1);
-        _nbCtrl.addNeighbor(2,3);
-        _nbCtrl.addNeighbor(3,2);
+        NeighborController l_nbCtrl = new NeighborController(d_gameContext);
+        l_nbCtrl.addNeighbor(1,2);
+        l_nbCtrl.addNeighbor(2,1);
+        l_nbCtrl.addNeighbor(2,3);
+        l_nbCtrl.addNeighbor(3,2);
 
-        MapService d_mapService = new MapService(d_gameContext);
-        Continent continent = d_gameContext.getContinents().get(1);
-        assertTrue(d_mapService.validateSubGraph(continent));
+        MapService l_mapService = new MapService(d_gameContext);
+        Continent l_continent = d_gameContext.getContinents().get(1);
+        assertTrue(l_mapService.validateSubGraph(l_continent));
     }
     /**
      * continentMap2 is not a connected subgraph
@@ -244,26 +243,26 @@ public class MapServiceTest {
     @Test
     public void validateContinentMap2() {
         //set map into d_gamecontext
-        d_gameContext.getContinents().put(1,new Continent(1,"North_America", 3, Color.RED ));
-        d_gameContext.getContinents().put(2,new Continent(2,"South_America", 5, Color.BLUE ));
+        d_gameContext.getContinents().put(1,new Continent(1,"North_America", 3, "RED" ));
+        d_gameContext.getContinents().put(2,new Continent(2,"South_America", 5, "BLUE" ));
 
-        CountryController _countryCtrl = new CountryController(d_gameContext);
-        Country canada = new Country(1,"Canada", 343,435, d_gameContext.getContinents().get(1) );
-        _countryCtrl.addCountry(1,1);
+        CountryController l_countryCtrl = new CountryController(d_gameContext);
+        new Country(1,"Canada", 343,435, d_gameContext.getContinents().get(1) );
+        l_countryCtrl.addCountry(1,1);
 
-        Country us = new Country(2,"US", 343,435, d_gameContext.getContinents().get(1) );
-        _countryCtrl.addCountry(2,1);
+        new Country(2,"US", 343,435, d_gameContext.getContinents().get(1) );
+        l_countryCtrl.addCountry(2,1);
 
-        Country mexco = new Country(3,"mexco", 343,435, d_gameContext.getContinents().get(1) );
-        _countryCtrl.addCountry(3,1);
+        new Country(3,"mexco", 343,435, d_gameContext.getContinents().get(1) );
+        l_countryCtrl.addCountry(3,1);
 
-        NeighborController _nbCtrl = new NeighborController(d_gameContext);
-        _nbCtrl.addNeighbor(1,2);
-        _nbCtrl.addNeighbor(2,1);
-        _nbCtrl.addNeighbor(2,3);
+        NeighborController l_nbCtrl = new NeighborController(d_gameContext);
+        l_nbCtrl.addNeighbor(1,2);
+        l_nbCtrl.addNeighbor(2,1);
+        l_nbCtrl.addNeighbor(2,3);
 
-        MapService d_mapService = new MapService(d_gameContext);
-        Continent continent = d_gameContext.getContinents().get(1);
-        assertFalse(d_mapService.validateSubGraph(continent));
+        MapService l_mapService = new MapService(d_gameContext);
+        Continent l_continent = d_gameContext.getContinents().get(1);
+        assertFalse(l_mapService.validateSubGraph(l_continent));
     }
 }
