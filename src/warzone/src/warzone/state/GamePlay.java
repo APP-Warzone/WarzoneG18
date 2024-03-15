7fffaf545f2457ee0e5bf62e68a56971f419ee60
Pritesh <pritesh.mangukiya604@gmail.com>
2024-03-15 18:23:43 -0400

    map migration State

--- src/warzone/src/warzone/state/GamePlay.java
+++ src/warzone/src/warzone/state/GamePlay.java
@@ -3,50 +3,78 @@ import warzone.service.*;
 import warzone.model.*;
 import warzone.view.*;
 
+import java.io.IOException;
+
 /**
- *	ConcreteState of the State pattern. In this example, defines behavior
- *  for commands that are valid in this state, and for the others signifies
- *  that the command is invalid.
+ *	ConcreteState of the State pattern. In this example, defines behavior 
+ *  for commands that are valid in this state, and for the others signifies  
+ *  that the command is invalid. 
  */
 public abstract class GamePlay extends Phase {
 
-    public GamePlay(GameEngine p_ge) {
-        super(p_ge);
-    }
-    abstract public void next();
+	public GamePlay(GameEngine p_ge) {
+		super(p_ge);
+	}
+	abstract public void next();
 
-    public void showMap() {
-        //todo
-    }
+	/**
+	 * Performs the action for user command: showmap
+	 *
+	 * Shows all countries and continents, armies on each country, ownership,
+	 * and connectivity in a way that enables efficient game play
+	 */
+	 public void showMap() {
+		 MapView.printMap(d_gameContext);
+		 MapView.printMapWithArmies(d_gameContext.getContinents());
+	 }	
+	
+	 public void addContinent(String p_parameters){
+		 printInvalidCommandMessage();
+	 }	
+	 public void removeContinent(String p_parameters) {
+		 printInvalidCommandMessage();
+	 }	
+	 public void addCountry (String p_parameters) {
+		 printInvalidCommandMessage();
+	 }	
+	 public void removeCountry(String p_parameters) {
+		 printInvalidCommandMessage();
+	 }	
+	 public void addNeighbor (String p_parameters) {
+		 printInvalidCommandMessage();
+	 }		
+	 public void removeNeighbor (String p_parameters) {
+		 printInvalidCommandMessage();
+	 }
 
-    public void addContinent(String p_parameters){
-        printInvalidCommandMessage();
-    }
-    public void removeContinent(String p_parameters) {
-        printInvalidCommandMessage();
-    }
-    public void addCountry (String p_parameters) {
-        printInvalidCommandMessage();
-    }
-    public void removeCountry(String p_parameters) {
-        printInvalidCommandMessage();
-    }
-    public void addNeighbor (String p_parameters) {
-        printInvalidCommandMessage();
-    }
-    public void removeNeighbor (String p_parameters) {
-        printInvalidCommandMessage();
-    }
+	/**
+	 * print invalid message and return false
+	 * @param p_fileName the filename
+	 * @return false
+	 */
+	public boolean saveMap (String p_fileName) {
+			printInvalidCommandMessage();
+			return false;
+	 }
 
-    public void saveMap (String p_fileName) {
-        printInvalidCommandMessage();
-    }
-    public void editMap (String p_fileName) {
-        printInvalidCommandMessage();
-    }
-    public void validateMap() {
-        printInvalidCommandMessage();
-    }
+	/**
+	 * print invalid message and return false
+	 * @param p_fileName the filename
+	 * @return false
+	 */
+	 public boolean editMap (String p_fileName) {
+		 printInvalidCommandMessage();
+		 return false;
+	 }
 
+	/**
+	 * print invalid message and return false
+	 * @return false
+	 */
+	 public boolean validateMap() {
+		 printInvalidCommandMessage();
+		 return false;
+	 }		
+	 
 
 }
--- src/warzone/src/warzone/state/Phase.java
+++ src/warzone/src/warzone/state/Phase.java
@@ -3,64 +3,92 @@ import warzone.service.*;
 import warzone.model.*;
 import warzone.view.*;
 
+import java.io.IOException;
+
 
 /**
- *	State of the State pattern. Here implemented as a abstract class.
+ *	State of the State pattern. Here implemented as a abstract class. 
+ *  
+ *	In this example, the states represent states in the board game Risk. 
+ *  There are many states, and even a hierarchy of states: 
  *
- *	In this example, the states represent states in the board game Risk.
- *  There are many states, and even a hierarchy of states:
- *
- *		Phase
+ *		Phase 
  *        MapEdit phase (abstract)
 
  *        GamePlay (abstract)
 
- *
- *      In each state, nextState() is defined so that it goes down in
+ *        
+ *      In each state, nextState() is defined so that it goes down in 
  */
 public abstract class Phase {
 
-    /**
-     *  Contains a reference to the State of the GameEngine
-     *  so that the state object can change the state of
-     *  the GameEngine to transition between states.
-     */
-    GameEngine d_gameEngine;
+	/**
+	 *  Contains a reference to the State of the GameEngine 
+	 *  so that the state object can change the state of 
+	 *  the GameEngine to transition between states. 
+	 */
+	GameEngine d_gameEngine;
+	
+	protected GameContext d_gameContext;
 
-    protected GameContext d_gameContext;
+	/**
+	 * Constructor for Phase
+	 * @param p_ge Game Engine
+	 */
+	Phase(GameEngine p_ge) {
+		d_gameEngine = p_ge;
+		d_gameContext = p_ge.getGameContext();
+	}
+	abstract public void addContinent(String p_parameters);
+	abstract public void removeContinent(String p_parameters);	
+	abstract public void addCountry (String p_parameters);
+	abstract public void removeCountry(String p_parameters);
 
-    Phase(GameEngine p_ge) {
-        d_gameEngine = p_ge;
-        d_gameContext = p_ge.getGameContext();
-    }
-    abstract public void addContinent(String p_parameters);
-    abstract public void removeContinent(String p_parameters);
-    abstract public void addCountry (String p_parameters);
-    abstract public void removeCountry(String p_parameters);
-    abstract public void showMap();
-    abstract public void saveMap (String p_fileName);
-    abstract public void editMap (String p_fileName);
-    abstract public void validateMap();
-    abstract public void addNeighbor (String p_parameters);
-    abstract public void removeNeighbor (String p_parameters);
-    abstract public void addPlayer(String p_playerName);
-    abstract public void removePlayer(String p_playerName);
-    abstract public void loadMap(String p_fileName);
-    abstract public void populatecountries();
+	/**
+	 * show map
+	 */
+	abstract public void showMap();
 
-    abstract public void reinforcement();
-    abstract public void issueOrder();
-    abstract public void executeOrder();
+	/**
+	 * save map
+	 * @param p_fileName file name
+	 * @return true if success. otherwise return false
+	 * @throws IOException io exception
+	 */
+	abstract public boolean saveMap (String p_fileName) throws IOException;
 
+	/**
+	 * edit map
+	 * @param p_fileName file name
+	 * @return true if success. otherwise return false
+	 */
+	abstract public boolean editMap (String p_fileName);
 
+	/**
+	 * validate map
+	 * @return true if success. otherwise return false
+	 */
+	abstract public boolean validateMap();
+	abstract public void addNeighbor (String p_parameters);	
+	abstract public void removeNeighbor (String p_parameters);	
+	abstract public void addPlayer(String p_playerName);	
+	abstract public void removePlayer(String p_playerName);	
+	abstract public void loadMap(String p_fileName);	
+	abstract public void populatecountries();
+	
+	abstract public void reinforcement(); 
+	abstract public void issueOrder();
+	abstract public void executeOrder();
+	
+	
 
-    // go to next phase
-    abstract public void next();
-
-    /**
-     *  Common method to all States.
-     */
-    public void printInvalidCommandMessage() {
-        System.out.println("Invalid command in state " + this.getClass().getSimpleName() );
-    }
+	// go to next phase
+	abstract public void next();
+	
+	/**
+	 *  Common method to all States. 
+	 */
+	public void printInvalidCommandMessage() {
+		System.out.println("Invalid command in state " + this.getClass().getSimpleName() );
+	}
 }
