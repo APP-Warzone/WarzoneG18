e97ca5a610efd48c2d0c702cd3d2a8e23a8ee4bc
Pritesh <pritesh.mangukiya604@gmail.com>
2024-03-15 18:30:08 -0400

    PlayerTest.java test

--- src/warzone/test/warzone/model/PlayerTest.java
+++ src/warzone/test/warzone/model/PlayerTest.java
@@ -19,115 +19,92 @@ import org.junit.Test;
  * tests for Player class
  */
 public class PlayerTest {
+    
+    /**
+     * make sure that player cannot deploy more armies that exceed their reinforcement pool
+     */
+    @Test
+    public void willNotDeployArmyExceedPool() {
+    	//arrange
+    	Player l_player = new Player("P1");
+    	l_player.setArmiesToDeploy(10);
 
-	/**
-	 * make sure that player cannot deploy more armies that exceed their reinforcement pool
-	 */
-	@Test
-	public void willNotDeployArmyExceedPool() {
-		//arrange
-		Player l_player = new Player("P1");
-		l_player.setArmiesToDeploy(10);
+    	Country l_country = new Country(1,"C1",0,0,null);
+    	l_country.setOwner(l_player);
+    	l_player.getConqueredCountries().put(l_country.getCountryID(), l_country);
+    	
+    	Order l_order = new DeployOrder(l_player, l_country , 20); 
+    	
+    	//act
+    	l_order.execute();
+    	
+    	//assert
+    	assertEquals(l_country.getArmyNumber(),10 );
+    }
+    
+    /**
+     * make sure that player deploy the same amount of army in the deploy order
+     */
+    @Test
+    public void willDeploySameArmyAsPool() {
+    	//arrange
+    	Player l_player = new Player("P1");
+    	l_player.setArmiesToDeploy(5);
 
-		Country l_country = new Country(1,"C1",0,0,null);
-		l_country.setOwner(l_player);
-		l_player.getConqueredCountries().put(l_country.getCountryID(), l_country);
+    	Country l_country = new Country(1,"C1",0,0,null);
+    	l_country.setOwner(l_player);
+    	l_player.getConqueredCountries().put(l_country.getCountryID(), l_country);
+    	
+    	Order l_order = new DeployOrder(l_player, l_country , 5); 
+    	
+    	//act
+    	l_order.execute();
+    	
+    	//assert
+    	assertEquals(l_country.getArmyNumber(),5 );
+    }
+    
+    /**
+     * make sure that player can not deploy the army to a country which the player does not own.
+     */
+    @Test
+    public void willNotDeployToOtherCountry() {
+    	//arrange
+    	Player l_player = new Player("P1");
+    	l_player.setArmiesToDeploy(5);
 
-		Order l_order = new DeployOrder(l_player, l_country , 20);
+    	Country l_country = new Country(1,"C1",0,0,null);
+    	
+    	Order l_order = new DeployOrder(l_player, l_country , 5); 
+    	
+    	//act
+    	assertEquals(l_order.execute(), false);
+    	
+    	//assert
+    	assertEquals(l_country.getArmyNumber(),0 );
+    }
+    
+    /**
+     * check if player can deploy a negative number of army to a country
+     */
+    @Test
+    public void willNotDeployNegativeArmy() {
+    	//arrange
+    	Player l_player = new Player("P1");
+    	l_player.setArmiesToDeploy(5);
 
-		//act
-		l_order.execute();
-
-		//assert
-		assertEquals(l_country.getArmyNumber(),10 );
-	}
-
-	/**
-	 * make sure that player deploy the same amount of army in the deploy order
-	 */
-	@Test
-	public void willDeploySameArmyAsPool() {
-		//arrange
-		Player l_player = new Player("P1");
-		l_player.setArmiesToDeploy(5);
-
-		Country l_country = new Country(1,"C1",0,0,null);
-		l_country.setOwner(l_player);
-		l_player.getConqueredCountries().put(l_country.getCountryID(), l_country);
-
-		Order l_order = new DeployOrder(l_player, l_country , 5);
-
-		//act
-		l_order.execute();
-
-		//assert
-		assertEquals(l_country.getArmyNumber(),5 );
-	}
-
-	/**
-	 * make sure that player can not deploy the army to a country which the player does not own.
-	 */
-	@Test
-	public void willNotDeployToOtherCountry() {
-		//arrange
-		Player l_player = new Player("P1");
-		l_player.setArmiesToDeploy(5);
-
-		Country l_country = new Country(1,"C1",0,0,null);
-
-		Order l_order = new DeployOrder(l_player, l_country , 5);
-
-		//act
-		assertEquals(l_order.valid(), false);
-
-		//assert
-		assertEquals(l_country.getArmyNumber(),0 );
-	}
-
-	/**
-	 * check if player can deploy a negative number of army to a country
-	 */
-	@Test
-	public void willNotDeployNegativeArmy() {
-		//arrange
-		Player l_player = new Player("P1");
-		l_player.setArmiesToDeploy(5);
-
-		Country l_country = new Country(1,"C1",0,0,null);
-		l_country.setOwner(l_player);
-		l_player.getConqueredCountries().put(l_country.getCountryID(), l_country);
-
-		Order l_order = new DeployOrder(l_player, l_country , -5);
-
-		//act
-		assertEquals(l_order.valid(), false);
-
-		//assert
-		assertEquals(l_country.getArmyNumber(),0 );
-	}
-
-	/**
-	 * check if conventOrder can generate the deploy order correctly
-	 */
-	@Test
-	public void willGenerateDeployOrder() {
-		//arrange
-		Player l_player = new Player("P1");
-		l_player.setArmiesToDeploy(5);
-
-		Country l_country = new Country(1,"C1",0,0,null);
-		l_country.setOwner(l_player);
-		l_player.getConqueredCountries().put(l_country.getCountryID(), l_country);
-		l_player.l_armyToIssue = 5;
-		l_player.l_armyHasIssued = 0;
-
-		//act
-		Order l_order = l_player.conventOrder("Deploy 1 2");
-		((DeployOrder)l_order).printOrder();
-
-		//assert
-		assertEquals(((DeployOrder)l_order).getArmyNumber(), 2);
-		assertEquals(((DeployOrder)l_order).getCountry(), l_country);
-	}
-
+    	Country l_country = new Country(1,"C1",0,0,null);
+    	l_country.setOwner(l_player);
+    	l_player.getConqueredCountries().put(l_country.getCountryID(), l_country);
+    	
+    	Order l_order = new DeployOrder(l_player, l_country , -5); 
+    	
+    	//act
+    	assertEquals(l_order.execute(), false);
+    	
+    	//assert
+    	assertEquals(l_country.getArmyNumber(),0 );
+    }
+    
+    
 }
