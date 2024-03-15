c0771930d291f320d4e7bff6a6f2bed8f5f081b8
harsh-hirapara <harshhirapara2000@gmail.com>
2024-03-15 10:32:21 -0400

    add comments to model files

--- src/warzone/src/warzone/model/GamePhase.java
+++ src/warzone/src/warzone/model/GamePhase.java
@@ -1,8 +1,13 @@
 package warzone.model;
 
+/**
+ * There are four phases in the game. They are map editing, startup, playing
+ * and finished respectively.
+ *
+ */
 public enum GamePhase {
-    MAPEDITING,
-    STARTUP,
-    PLAYING,
-    FINISHED
+	MAPEDITING,	
+	STARTUP,
+	PLAYING,
+	FINISHED
 }
