bd440e71e84d587685ef356539d4c3fb35d2489b
Khushi2111 <82765191+Khushi2111@users.noreply.github.com>
2024-03-16 00:27:44 -0400

    Updated

--- src/warzone/test/warzone/model/BlockadeOrderTest.java
+++ src/warzone/test/warzone/model/BlockadeOrderTest.java
@@ -12,7 +12,7 @@ import org.junit.Test;
  * Tests for BlockadeOrder class
  */
 public class BlockadeOrderTest {
-	
+
 	/**
 	 * This method tests a valid order
 	 */
@@ -21,14 +21,14 @@ public class BlockadeOrderTest {
 		Player l_player=new Player("player1");
 		Country l_country=new Country(0,"country1");
 		BlockadeOrder l_order=new BlockadeOrder(l_player, l_country);
-		assert(l_order.valid()==false);
-		l_country.setOwner(l_player);
+		assertFalse(l_order.valid());
+		l_country.setCountryState(CountryState.Occupied, l_player);
 		l_player.getConqueredCountries().put(l_country.getCountryID(), l_country);
-		assert(l_order.valid()==true);
+		assertTrue(l_order.valid());
 		l_player.setIsAlive(false);
-		assert(l_order.valid()==false);
+		assertFalse(l_order.valid());
 	}
-	
+
 	/**
 	 * Invalid order if target country not belong to player
 	 */
@@ -39,7 +39,7 @@ public class BlockadeOrderTest {
 		BlockadeOrder l_order=new BlockadeOrder(l_player, l_country);
 		assertFalse(l_order.valid());
 	}
-	
+
 	/**
 	 * Invalid order if player dead
 	 */
@@ -48,13 +48,13 @@ public class BlockadeOrderTest {
 		Player l_player=new Player("player1");
 		Country l_country=new Country(0,"country1");
 		BlockadeOrder l_order=new BlockadeOrder(l_player, l_country);
-		l_country.setOwner(l_player);
+		l_country.setCountryState(CountryState.Occupied, l_player);
 		l_player.getConqueredCountries().put(l_country.getCountryID(), l_country);
 		assert(l_order.valid());
 		l_player.setIsAlive(false);
 		assertFalse(l_order.valid());
 	}
-	
+
 	/**
 	 * blockade order will execute success
 	 */
@@ -64,13 +64,14 @@ public class BlockadeOrderTest {
 		Country l_country=new Country(0,"country1");
 		l_country.setArmyNumber(4);
 		BlockadeOrder l_order=new BlockadeOrder(l_player, l_country);
-		l_country.setOwner(l_player);
+		l_country.setCountryState(CountryState.Occupied, l_player);
 		l_player.getConqueredCountries().put(l_country.getCountryID(), l_country);
 		l_order.execute();
 		assertEquals(l_country.getArmyNumber(),12);
+		assertEquals( l_country.getCountryState(), CountryState.Neutral);
 	}
-	
-	
+
+
 	/**
 	 * check if success to generate the blockade order with right command
 	 */
@@ -78,8 +79,9 @@ public class BlockadeOrderTest {
 	public void willCreateBlockadeOrder() {
 		Player l_player = new Player("P1");
 		Country l_country = new Country(1,"C1",0,0,null);
+		GameContext.getGameContext().getCountries().put(l_country.getCountryID(),l_country);
 		l_country.setArmyNumber(5);
-		l_country.setOwner(l_player);
+		l_country.setCountryState(CountryState.Occupied, l_player);
 		l_player.getConqueredCountries().put(l_country.getCountryID(), l_country);
 		l_player.getCards().add(Card.BLOCKADE);
 		Order l_order = l_player.conventOrder("blockade 1");
@@ -93,28 +95,28 @@ public class BlockadeOrderTest {
 	public void testValid() {
 		Player l_player=new Player("player1");
 		Country l_country=new Country(1,"country1");
-		
+
 		//act
 		Order l_order=new BlockadeOrder(l_player, l_country);
-		
+
 		//assert
 		assertFalse(l_order.valid());
-  }  
-	
+	}
+
 	/**
 	 * This method tests the valid method of BlockadeOrder class
 	 */
 	@Test
 	public void willFailedWhenNullCountry() {
 		Player l_player=new Player("player1");
-		
+
 		//act
 		Order l_order=new BlockadeOrder(l_player, null);
-		
+
 		//assert
 		assertFalse(l_order.valid());
 	}
-	
+
 	/**
 	 * This method tests the valid method of BlockadeOrder class
 	 */
@@ -122,15 +124,15 @@ public class BlockadeOrderTest {
 	public void willTrueWhenTargetCountryIsTheSameOwner() {
 		Player l_player=new Player("player1");
 		Country l_country=new Country(1,"country1");
-		l_country.setOwner(l_player);
-		
+		l_country.setCountryState(CountryState.Occupied, l_player);
+
 		//act
 		Order l_order=new BlockadeOrder(l_player, l_country);
-		
+
 		//assert
 		assertTrue(l_order.valid());
 	}
-	
+
 	/**
 	 * This method tests the valid method of BlockadeOrder class
 	 */
@@ -139,18 +141,18 @@ public class BlockadeOrderTest {
 		Player l_player=new Player("player1");
 		Player l_player2=new Player("player2");
 		Country l_country=new Country(1,"country2");
-		l_country.setOwner(l_player2);
-		
+		l_country.setCountryState(CountryState.Occupied, l_player2);
+
 		GameContext l_gameContext = GameContext.getGameContext();
 		NegotiateOrder l_negotiateOrder = new NegotiateOrder(l_player, l_player2 );
 		l_gameContext.addDiplomacyOrderToList(l_negotiateOrder);
-		
-		
+
+
 		//act
 		Order l_order=new BlockadeOrder(l_player, l_country);
-		
+
 		//assert
 		assertFalse(l_order.valid());
 	}
-	
+
 }
--- src/warzone/test/warzone/model/NegotiateOrderTest.java
+++ src/warzone/test/warzone/model/NegotiateOrderTest.java
@@ -26,7 +26,7 @@ public class NegotiateOrderTest {
 		assert(l_order != null);
 		assert(l_player.getCards().contains(Card.NEGOTIATE) == false);
 	}
-	
+
 	/**
 	 * check if failed to generate the Negotiate order Without A Card
 	 */
@@ -39,7 +39,7 @@ public class NegotiateOrderTest {
 		Order l_order = l_player.conventOrder("negotiate p2");
 		assert(l_order == null);
 	}
-	
+
 	/**
 	 * check if failed to generate the Negotiate order Without A right layerName
 	 */
@@ -52,7 +52,7 @@ public class NegotiateOrderTest {
 		Order l_order = l_player.conventOrder("negotiate p23434");
 		assert(l_order == null);
 	}
-	
+
 	/**
 	 * check if order is Valid
 	 */
@@ -66,5 +66,30 @@ public class NegotiateOrderTest {
 		Order l_order = l_player.conventOrder("negotiate p2");
 		Boolean l_result = l_order.valid();
 		assert(l_result);
-	}	
+	}
+
+	/**
+	 * check if game engine will Update Context After Execution
+	 */
+	@Test
+	public void willUpdateContextAfterExecution() {
+		//arrange
+		Player l_player = new Player("P1");
+		Player l_player2 = new Player("p2");
+		Player l_player3 = new Player("p3");
+		GameContext l_gameContext = GameContext.getGameContext();
+		l_gameContext.getPlayers().put(l_player2.getName(), l_player2);
+		l_player.getCards().add(Card.NEGOTIATE);
+		Order l_order = l_player.conventOrder("negotiate p2");
+
+		//act
+		l_order.execute();
+
+		//assert
+		assert(l_gameContext.isDiplomacyInCurrentTurn(l_player,l_player2));
+		assert(l_gameContext.isDiplomacyInCurrentTurn(l_player2,l_player));
+		assertFalse(l_gameContext.isDiplomacyInCurrentTurn(l_player,l_player3));
+	}
+
+
 }
