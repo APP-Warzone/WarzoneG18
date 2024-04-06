0d4df8e55327955436f2e6a4c9a67274af97a969
Vrushabh Savani <vrushabhsavani123@gmail.com>
2024-04-06 01:18:40 -0400

    refactoring

--- src/warzone/test/warzone/service/ConquestMapWriterTest.java
+++ src/warzone/test/warzone/service/ConquestMapWriterTest.java
@@ -1,133 +1,100 @@
 package warzone.service;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Map.Entry;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-import warzone.model.Continent;
-import warzone.model.Country;
+import java.io.IOException;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+
 import warzone.model.GameContext;
-import warzone.model.LogEntryBuffer;
+import warzone.model.Router;
+import warzone.state.MapEditor;
 
 /**
- * Conquest Map Writer
+ * test cases for ConquestMapWriter
+ * @author zexin
+ *
  */
-public class ConquestMapWriter {
+public class ConquestMapWriterTest {
     /**
-     * game context
+     * gamecontext before each test
      */
-    private GameContext d_gameContext;
+    GameContext d_gameContext;
 
     /**
-     * log entry buffer
+     * conquestMapWriter
      */
-    private LogEntryBuffer d_logEntryBuffer;
+    ConquestMapWriter d_conquestMapWriter;
 
     /**
-     * Map Service
+     * conquestMapReader
      */
-    private MapService d_mapService;
-
+    ConquestMapReader d_conquestMapReader;
 
     /**
-     * The constructor of this class
-     * @param p_gameContext the current game context
+     * clear the gamecontext before each test
      */
-    public ConquestMapWriter(GameContext p_gameContext) {
-        d_gameContext = p_gameContext;
-        d_logEntryBuffer = new LogEntryBuffer(p_gameContext);
-        d_mapService = new MapService(p_gameContext);
+    @Before
+    public void beforeEachTetCase(){
+        GameContext.clear();
+        d_gameContext = GameContext.getGameContext();
+        d_conquestMapWriter = new ConquestMapWriter(d_gameContext);
+        d_conquestMapReader = new ConquestMapReader(d_gameContext);
+        GameEngine.getGameEngine(d_gameContext).setPhase(new MapEditor(GameEngine.getGameEngine(d_gameContext)));
+        d_gameContext.setCurrentRouter(new Router(null, null, null, "testCommand"));
     }
 
     /**
-     * Save a map to a text file exactly as edited (using the "conquest" game map format).
-     * @param p_fileName the filename
-     * @return true if successfully save the map, otherwise return false
-     * @throws IOException the exception of saving files
+     * clear the gamecontext after this class run
      */
-    public boolean saveConquestMap(String p_fileName) throws IOException {
-        // validate if the filename is legal
-        if(p_fileName == null || p_fileName.trim().isEmpty() || p_fileName.trim().length() > 20 ) {
-            d_logEntryBuffer.logAction("ERROR", "InValid File Name, please type a valid file name, with length less than 20.");
-            return false;
-        }
-
-        if(! d_mapService.validateMap() ) {
-            d_logEntryBuffer.logAction("ERROR", "InValid map, please check the map.");
-            return false;
-        }
-
-        // call mapService to save the map and return the path
-        p_fileName = p_fileName.trim();
-        try{
-            if(saveMap(p_fileName)) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-        catch(Exception ex) {
-            d_logEntryBuffer.logAction("ERROR", "Exception occured when saving the map. " + ex.toString());
-            return false;
-        }
+    @AfterClass
+    public static void afterClass() {
+        GameContext.clear();
     }
 
     /**
-     * This method will save the map
-     * @param p_fullFileName the name of the file that should be saved
-     * @return true if success
-     * @throws Exception the exceptions
+     * test save map function
+     * @throws IOException io exception
      */
-    private boolean saveMap(String p_fullFileName) throws Exception {
-        try{
-            String l_fileName ;
-            if(p_fullFileName.indexOf(".") > -1)
-                l_fileName = p_fullFileName.substring(0,p_fullFileName.indexOf("."));
-            else
-                l_fileName = p_fullFileName;
-            String l_path = this.d_gameContext.getMapfolder();
+    @Test
+    public void testSaveConquestMap2() throws IOException {
+        assertTrue(d_conquestMapReader.loadConquestMap("starwar.map"));
+        int l_continentNum = d_gameContext.getContinents().size();
+        int l_countryNum = d_gameContext.getCountries().size();
 
-            //build the content using StringBuilder
-            StringBuilder l_map = new StringBuilder();
-            // map part
-            l_map.append("[Map]");
-            l_map.append("\nauthor=the 6441 Super Team");
-            l_map.append("\nimage=" + l_fileName + ".bmp");
-            l_map.append("\nwrap=no");
-            l_map.append("\nscroll=no");
-            l_map.append("\nwarn=no");
+        assertTrue(d_conquestMapWriter.saveConquestMap("starwar-test.map"));
+        assertEquals(d_gameContext.getContinents().size(), l_continentNum);
+        assertEquals(d_gameContext.getCountries().size(), l_countryNum);
+    }
 
-            // continents part
-            l_map.append("\n");
-            l_map.append("\n[Continents]");
-            for (Entry<Integer, Continent> l_entry: d_gameContext.getContinents().entrySet()) {
-                l_map.append("\n" + l_entry.getValue().getContinentName() + "=" + l_entry.getValue().getBonusReinforcements());
-            }
+    /**
+     * test save map function in wrong file name "This-is-not-an-valid-file-name.map"
+     * @throws IOException io exception
+     */
+    @Test
+    public void testSaveConquestMapInvalidFileName1() throws IOException {
+        assertFalse(d_conquestMapWriter.saveConquestMap("This-is-not-an-valid-file-name.map"));
+    }
 
-            // territories part
-            l_map.append("\n");
-            l_map.append("\n[Territories]");
-            for (Entry<Integer, Country> l_entry : d_gameContext.getCountries().entrySet()) {
-                Country l_country = l_entry.getValue();
-                l_map.append("\n" + l_country.getCountryName() + "," + l_country.getXPosition() + "," + l_country.getYPosition()
-                        + "," + l_country.getContinent().getContinentName());
-                for (Entry<Integer, Country> l_neighbors : l_country.getNeighbors().entrySet()) {
-                    l_map.append("," + l_neighbors.getValue().getCountryName());
-                }
-            }
-            l_map.append("\n");
+    /**
+     * test save map function null file name
+     * @throws IOException io exception
+     */
+    @Test
+    public void testSaveConquestMapInvalidFileName2() throws IOException {
+        assertFalse(d_conquestMapWriter.saveConquestMap(null));
+    }
 
-            //write the content into the map
-            BufferedWriter writer = new BufferedWriter(new FileWriter(l_path + p_fullFileName));
-            writer.write(l_map.toString());
-
-            writer.close();
-            return true;
-        }
-        catch(Exception ex) {
-            throw ex;
-        }
+    /**
+     * test save map function with empty file name
+     * @throws IOException io exception
+     */
+    @Test
+    public void testSaveConquestMapInvalidFileName3() throws IOException {
+        assertFalse(d_conquestMapWriter.saveConquestMap(""));
     }
 }
