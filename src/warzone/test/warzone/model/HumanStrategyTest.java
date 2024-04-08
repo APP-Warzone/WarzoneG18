6090c5d4da590f094b62c44d77b68fe2dfe74628
harsh-hirapara <harshhirapara2000@gmail.com>
2024-04-08 02:47:33 -0400

    refactor map handler

--- src/warzone/test/warzone/model/HumanStrategyTest.java
+++ src/warzone/test/warzone/model/HumanStrategyTest.java
@@ -1,10 +1,24 @@
 package warzone.model;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * test cases for HumanStrategy
- * @author Harsh
+ * @author zexin
  *
  */
 public class HumanStrategyTest {
 
+    /**
+     * test if it is a human strategy
+     */
+    @Test
+    public void testIfStrategyCorrect(){
+
+        Player l_player = new Player("p1");
+
+        assertEquals(l_player.getPlayerStrategyType(), PlayerStrategyType.HUMAN);
+    }
 }
